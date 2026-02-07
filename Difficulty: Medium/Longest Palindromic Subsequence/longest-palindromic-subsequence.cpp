f4df2ab27a26dfed32bd6e5ac38eefe307276f11
Sharmaji2327 <abhikashi1234@gmail.com>
2026-02-07 16:11:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Palindromic Subsequence/longest-palindromic-subsequence.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+  int t[1001][1001];
+  int solve(int i,int j,string &s){
+      if(i>j)return 0;
+      if(i==j)return 1;
+      if(t[i][j]!=-1)return t[i][j];
+      if(s[i]==s[j]){
+          return 2+solve(i+1,j-1,s);
+      }
+      return t[i][j]=max(solve(i+1,j,s),solve(i,j-1,s));
+  }
+    int longestPalinSubseq(string &s) {
+        // code here
+        memset(t,-1,sizeof(t));
+        return solve(0,s.size()-1,s);
+    }
+};
