d9986800bed7b247f485aab838af644ee5e594d9
Sharmaji2327 <abhikashi1234@gmail.com>
2026-02-12 11:34:03 +0530

    Time: 0 ms (100.00%) | Memory: 16.2 MB (64.91%) - LeetSync

--- 740-delete-and-earn/delete-and-earn.cpp
+++ 740-delete-and-earn/delete-and-earn.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
-int t[20001];
-int solve(int i,vector<int>&nums){
-    if(i>=nums.size())return 0;
-    if(t[i]!=-1)return t[i];
-    int take=nums[i]+solve(i+2,nums);
-    int skip=solve(i+1,nums);
-    return t[i]=max(take,skip);
+int solve(int i,vector<int>&arr,vector<int>&dp){
+    if(i>=arr.size())return 0;
+    if(dp[i]!=-1)return dp[i];
+    int take=arr[i]+solve(i+2,arr,dp);
+    int skip=solve(i+1,arr,dp);
+    return dp[i]=max(take,skip);
 }
-    int deleteAndEarn(vector<int>& arr) {
-        
-        int maxi=*max_element(arr.begin(),arr.end());
-        vector<int>mp(maxi+1,0);
-        for(auto x:arr){
-            mp[x]+=x;
+    int deleteAndEarn(vector<int>& nums) {
+        int maxi=*max_element(nums.begin(),nums.end());
+        vector<int>arr(maxi+1,0);
+        for(auto i:nums){
+            arr[i]+=i;
         }
-        memset(t,-1,sizeof(t));
-        return solve(0,mp);
+        vector<int>dp(arr.size()+1,-1);
+        return solve(0,arr,dp);
     }
 };
